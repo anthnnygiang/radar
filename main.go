95a14cdd840499409a48ad059574ef9f50b42587
Anthony Giang <anthnnygiang@icloud.com>
2024-11-10 15:00:42 +1100

    usable

--- main.go
+++ main.go
@@ -7,46 +7,56 @@ import (
 	"os"
 )
 
-/* Strategy:
-Read top 1,000,000 domains
-Add them into a set
-Read top 500,000 domains
-If domain is in the 1,000,000 set
-then remove it and add it to the 500,000 set
-repeat.
+/* strategy:
+unique domain -> bucket
 */
 
-func processFile(filename string) {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-}
-
 func main() {
-	files, err := os.ReadDir("./data")
-	if err != nil {
-		log.Fatal(err)
-	}
+	dataDir := "./data"
+	fileNameOrder := []string{"200", "500", "1000", "2000", "5000", "10000", "20000", "50000", "100000", "200000", "500000", "1000000"}
+	baseFileName := fmt.Sprintf("%s/top-%s-domains.csv", dataDir, fileNameOrder[0])
 
-	for _, file := range files {
-		fmt.Println(file.Name())
-	}
-
-	reader := csv.NewReader(file)
+	baseFile, err := os.Open(baseFileName)
+	reader := csv.NewReader(baseFile)
 	records, err := reader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	set := make(map[string]struct{})
-
+	// add each record into the set
+	domainsMap := make(map[string]string)
 	for _, row := range records {
 		for _, field := range row {
-			fmt.Println(field)
-			set[field] = struct{}{}
+			domainsMap[field] = "200"
 		}
 	}
 
+	for _, order := range fileNameOrder {
+		// skip the baseFile
+		if order == fileNameOrder[0] {
+			continue
+		}
+
+		// read each file
+		currFileName := fmt.Sprintf("%s/top-%s-domains.csv", dataDir, order)
+		fmt.Sprintln("Reading file:", currFileName)
+		currFile, err := os.Open(currFileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		reader := csv.NewReader(currFile)
+		records, err := reader.ReadAll()
+		for _, row := range records {
+			for _, field := range row {
+				// if not already in the map, then add it with the next group
+				if _, exists := domainsMap[field]; !exists {
+					domainsMap[field] = order
+				}
+
+			}
+		}
+	}
+
+	for k, v := range domainsMap {
+		fmt.Printf("%-*s: %s\n", 60, k, v)
+	}
 }
