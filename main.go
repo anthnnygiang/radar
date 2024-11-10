635199414ecd03a8119ed5bd4ee80b7ccd1bf3cc
Anthony Giang <anthnnygiang@icloud.com>
2024-11-10 16:49:41 +1100

    add defers

--- main.go
+++ main.go
@@ -20,35 +20,40 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
 
 	// read each file
 	for _, bracket := range brackets {
-		currFileName := filepath.Join(dataDir, fmt.Sprintf(fileFormat, bracket))
-		fmt.Println("Reading file:", currFileName)
-		currFile, err := os.Open(currFileName)
-		if err != nil {
-			log.Fatal(err)
-		}
-		reader := csv.NewReader(currFile)
-		// read the first row to ignore headers
-		_, err = reader.Read()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		records, err := reader.ReadAll()
-		for _, row := range records {
-			for _, field := range row {
-				// if not already in the map, then add it and write to file
-				if _, exists := domainsMap[field]; !exists {
-					domainsMap[field] = bracket
-					_, err := file.Write([]byte(p.Sprintf("%*d: %s\n", 7, bracket, field)))
-					if err != nil {
-						log.Fatal(err)
-					}
-				}
-
+		func(bracket int) {
+			currFileName := filepath.Join(dataDir, fmt.Sprintf(fileFormat, bracket))
+			fmt.Println("Reading file:", currFileName)
+			currFile, err := os.Open(currFileName)
+			if err != nil {
+				log.Fatal(err)
 			}
-		}
+			defer currFile.Close()
+
+			reader := csv.NewReader(currFile)
+			// read the first row to ignore headers
+			_, err = reader.Read()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			records, err := reader.ReadAll()
+			for _, row := range records {
+				for _, field := range row {
+					// if not already in the map, then add it and write to file
+					if _, exists := domainsMap[field]; !exists {
+						domainsMap[field] = bracket
+						_, err := file.Write([]byte(p.Sprintf("%*d: %s\n", 7, bracket, field)))
+						if err != nil {
+							log.Fatal(err)
+						}
+					}
+
+				}
+			}
+		}(bracket)
 	}
 }
